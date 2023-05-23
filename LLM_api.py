ef20f2778eeef8c1bc9c49318d18f0bf6364314d
Osama Saad <32080534+uasmasa3d@users.noreply.github.com>
2023-05-23 21:24:34 +0300

    Update LLM_api.py
    More linting errors fixed

--- LLM_api.py
+++ LLM_api.py
@@ -6,11 +6,13 @@ capabilities, including text generation, question answering, and sentiment
 analysis. This module provides a simple interface for making API requests
 and processing the results.
 """
-
 import poe
+
 from Image_captioning import generate_caption
+
 client = poe.Client("POE_TOKEN")
-has_image= True
+has_image = True
+
 
 def concatenate_strings(string_list):
     """
@@ -22,72 +24,82 @@ def concatenate_strings(string_list):
     Returns:
         str: A string containing all the input strings concatenated together.
     """
-    concatenated_string = ''.join(string_list)
-    return concatenated_string
 
-def reply(message,has_image,sample_img):
+    return ''.join(string_list)
+
+
+def reply(message, has_image, sample_img):
     """
-    Sends a message to the LLM API and returns the response as a concatenated string.
+    Sends a message to the LLM API and returns the response as a \
+        concatenated string.
 
-    If `has_image` is True, generates a caption for `sample_img` using the `generate_caption`
-    function, and adds it to the beginning of the `message` before sending it to the API.
+    If `has_image` is True, generates a caption for `sample_img` using the \
+        `generate_caption` function, and adds it to the beginning of the\
+              `message` before sending it to the API.
 
     Args:
         message (str): The message to send to the LLM API.
         has_image (bool): Whether or not `sample_img` is provided.
-        sample_img (PIL.Image.Image): An image to generate a caption for, if `has_image` is True.
+        sample_img (PIL.Image.Image): An image to generate a caption  \
+              for, if `has_image` is True.
 
     Returns:
-        str: The concatenated response from the LLM API, with any header information removed.
+        str: The concatenated response from the LLM API, with any  \
+            header information removed.
     """
     if has_image:
         caption = generate_caption(sample_img)
-        message =  "assume that I showed you a photo that: " + caption \
-                   + ". now, given the information about the \
+        message = "assume that I showed you a photo that: " + caption  \
+            + ". now, given the information about the  \
                     photo, I'll tell you this: " + message
     outlist = []
     for chunk in client.send_message("nutria", message):
         outlist.append(chunk["text_new"])
-    del outlist[0]
+    del outlist[0]  # Remove header
     stringlist = concatenate_strings(outlist)
     return stringlist
 # capybara for sage
 
-#not stable
-def reply_with_photo(message,has_image,sample_img):
+# Not stable
+
+
+def reply_with_photo(message, has_image, sample_img):
     """
     Sends a message to the LLM API and returns the response as a string.
 
-    If `has_image` is True, generates a caption for `sample_img` using the `generate_caption`
-    function, and adds it to the beginning of the `message` before sending it to the API.
+    If `has_image` is True, generates a caption for `sample_img` using the  \
+        `generate_caption` function, and adds it to the beginning of the  \
+            `message` before sending it to the API.
 
-    After sending the message to the API, extracts information from the response to determine
-    whether or not the user requested an image. If an image is requested, calls a function to
-    retrieve the image and returns it along with the text response.
+    After sending the message to the API, extracts information from the  \
+        response to determine whether or not the user requested an image.  \
+            If an image is requested, calls a function to retrieve the image  \
+                and returns it along with the text response.
 
     Args:
         message (str): The message to send to the LLM API.
         has_image (bool): Whether or not `sample_img` is provided.
-        sample_img (PIL.Image.Image): An image to generate a caption for, if `has_image` is True.
+        sample_img (PIL.Image.Image): An image to generate a caption for,\
+              if `has_image` is True.
 
     Returns:
-        str: If no image is requested, returns the text response from the LLM API as a string.
-        If an image is requested, returns a tuple containing the text response and the image.
+        str: If no image is requested, returns the text response from the LLM\
+              API as a string. If an image is requested, returns a tuple \
+                containing the text response and the image.
     """
     if has_image:
         caption = generate_caption(sample_img)
-        message =  "assume that I showed you a photo that: " + caption \
-               + ". now, given the information about the photo, I'll \
+        message = "assume that I showed you a photo that: " + caption \
+            + ". now, given the information about the photo, I'll \
                 tell you this: " + message
     output_list = []
-    result = []
     for chunk in client.send_message("nutria", message):
         output_list.append(chunk["text_new"])
     my_string = "".join(output_list)
     my_list = my_string.split("\n")
 
     # Calling google search api
-    if my_list[1]=="Yes":
+    if my_list[1] == "Yes":
         # use my_list[3] as it contains the description of \
         # the photo required and you have to make a function\
         # that returns an image and put it in the return of the reply function.
